bc5e39f1bd42e95ebb3a3c614dec9b23e3b59f4d
dustinle48 <ducthinh481994@gmail.com>
2022-06-19 19:17:56 -0400

    Change socket.io base url

--- src/context/socket.js
+++ src/context/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 import React from "react";
 
-export const socket = io(window.location.origin);
+export const socket = io("https://karaoke-budget-backend.herokuapp.com");
 socket.on("connection", () => {
     console.log("connected to server");
 });
