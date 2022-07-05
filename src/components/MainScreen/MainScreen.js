6ff008f3121d027cae385239d2c18608a3e311eb
dustinle48 <ducthinh481994@gmail.com>
2022-07-05 13:58:49 -0400

    Add show player switch

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Grid, Switch, Typography } from '@mui/material';
 
 import { YoutubePlayer } from "./index";
 
@@ -8,14 +8,40 @@ const styles = {
     height: "100%",
     width: "100%",
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    height: "30px",
+    width: "200px",
+    borderRadius: "30px",
+    marginBottom: "20px",
+    backgroundColor: "#ffffff",
+    opacity: 0.9,
+    padding: "3rem 1rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   }
 };
 
 export default function MainScreen({playList, connectedRoom}) {
+  const [player, setPlayer] = useState(false);
+
+  const handleSwitch = () => {
+    setPlayer(!player)
+  }
+
   return (
     <Grid sx={styles.root}>
-      <YoutubePlayer song={playList[0]} connectedRoom={connectedRoom} />
+      <Box sx={styles.box}>
+        <Switch color='warning' onChange={handleSwitch}/>
+        <Typography>Show player</Typography>
+      </Box>
+      
+      { player && <YoutubePlayer song={playList[0]} connectedRoom={connectedRoom} />
+      }
     </Grid>
   )
 }
